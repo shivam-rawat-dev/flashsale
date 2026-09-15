e4178cbb029ebd957e2fea5243a17daf5d67790a
shivam-rawat-dev <shivamr7860@gmail.com>
2026-09-15 19:38:37 +0530

    fix(kafka): configure robust ErrorHandlingDeserializer and type mapping for consumer factory

--- flashsale/src/main/java/com/enterprise/flashsale/config/KafkaConsumerConfig.java
+++ flashsale/src/main/java/com/enterprise/flashsale/config/KafkaConsumerConfig.java
@@ -98,10 +98,13 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class.getName());
         props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.enterprise.flashsale.*");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.enterprise.flashsale.dto.FlashSaleOrderEvent");
 
         return new DefaultKafkaConsumerFactory<>(props);
     }
